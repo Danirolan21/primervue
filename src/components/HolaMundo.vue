9faf5e24ee13d2e76a1c77d565ef52ef32311277
Danirolan21 <danirolan21@hotmail.com>
2024-10-24 16:31:50 +0200

    MiPrimeraInteracciónVue

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Mi primera app de Vue</h1>
+    </div>
+</template>
+
+<script>
+    //ESTO ES CODIGO JS (ESPECIAL)
+    //CADA COMPONENT QUE CREEMOS SIEMPRE DEBEMOS EXPORTARLO
+    //CON UN name
+    export default {
+        name: "HolaMundo"
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: blue;
+    }
+</style>
